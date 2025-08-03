1704f179a1bfac65474ab54957edb16f6471dd3c
PhanTuanAnh2009 <130231245+PhanTuanAnh2009@users.noreply.github.com>
2025-08-03 17:01:36 +0700

    jljlk

--- store/static/store/js/script.js
+++ store/static/store/js/script.js
@@ -6,3 +6,21 @@ function showAlert(name) {
 
 
 const chatbot=document.querySelector("#chatbot")
+document.getElementById('product-form').addEventListener('submit',function(e){
+  e.preventDefault
+  var form=e.target
+  var formData=new FormData(form)
+  fetch('api/products/create',{
+    method:'POST',
+    headers:{
+      'X-CSRFToken':'{{csrf_token}}',
+    },
+    body:formData,
+  })
+  .then(response=>response.json())
+  .then(data=>{alert('Them thanh cong san pham')})
+  .catch(error=>{
+    console.error('Error:',error);
+    alert("Xay ra loi khi them san pham");
+  })
+})
